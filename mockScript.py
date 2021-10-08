a7da63b596c2607e84cee9b17794748357161d0c
FrankC22 <frankcar203@aol.com>
2021-10-08 01:59:12 -0400

    Updated tests script for sprint1

--- mockScript.py
+++ mockScript.py
@@ -14,33 +14,28 @@ lMotor.start(5)
 rMotor.start(5)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Turn right on LightBlue
 # while(True):
 #     color = colorSensor.get_color()
-#     if color  == "Violet":
-#         lMotor.start(-5)
-#         rMotor.start(5)
-#     # elif color == "":
-
-#     # elif color == "":  
+#     if color == "Light Blue":
+#         lMotor.start(15)
         
+#Turn left on Blue
+# while(True):
+#     color = colorSensor.get_color()
+#     if color == "Blue":
+#         rMotor.start(15)
 
 
+# #Go backwards on Green
+# while(True):
+#     color = colorSensor.get_color()
+#     if color == "Green":
+#         rMotor.start(-10)
+#         lMotor.start(-10)
 
+while(True):
+    color = colorSensor.get_color()
+    if color  == "Violet":
+        lMotor.start(-5)
+        rMotor.start(5)
