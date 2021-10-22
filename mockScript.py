13a01502ab9c1762e548aa274336cd5e63c3f046
Francis <francis.cartelle@gmail.com>
2021-10-22 12:01:22 -0400

    Redacted MockScript for sprint2

--- mockScript.py
+++ mockScript.py
@@ -4,34 +4,15 @@ from spike import PrimeHub,Motor,ColorSensor,DistanceSensor
 hub = PrimeHub()
 
 cs = ColorSensor("A")
-ds = DistanceSensor("A")
+
 
 
 lMotor = Motor("A")
 rMotor = Motor("B")
 
 
-while(ds.get_distance_cm() > 20):
-    lMotor.start(15)
-    rMotor.start(15)
-    
-
-while(hub.motion_sensor.get_yaw_angle() < 90 or hub.motion_sensor.get_yaw_angle() > 350):
-    lMotor.start(10)
-    rMotor.start(-10)
-
-while(ds.get_distance_cm() > 20):
-    lMotor.start(20)
-    rMotor.start(20)
-
-
-while(hub.motion_sensor.get_yaw_angle() > 2):
-    lMotor.start(-5)
-    rMotor.start(5)
-
-lMotor.start(100)
-rMotor.start(100)
-
+rMotor.start(15)
+lMotor.start(15)
 
     
 
